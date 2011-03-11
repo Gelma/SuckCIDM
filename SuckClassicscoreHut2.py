37a7a329d172ea9c4e890a308a0d3b53672df7af
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-03-11 11:38:33 +0100

    Varie migliorie

--- SuckClassicscoreHut2.py
+++ SuckClassicscoreHut2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import string, urllib2, os, urllib
+import string, urllib2, os, urllib, socket, time, shutil
 try:
 	from BeautifulSoup import BeautifulSoup
 except:
@@ -9,20 +9,32 @@ except:
 	import sys
 	sys.exit()
 
+socket.setdefaulttimeout(35)
+tank='/home/mic/'
+
 for lettera in string.letters[26:]:
 	print 'Elaboro lettera',lettera
 	url_base='http://www.classicscore.hut2.ru/%s.html' % (lettera,)
 
+	print 'fetch'
 	html = urllib2.urlopen(url_base).read()
 
 	s=BeautifulSoup(html)
 
-for tag in s.findAll('a'):
+	print 'tag'
+	for tag in s.findAll('a'):
 	url = tag.attrs[0][1]
 	nome = tag.text+'.pdf'
 	if 'scorage' in url:
-		print 'Scarico',url
-		try:
-			urllib.urlretrieve(url,'/tmp/'+nome)
-		except:
-			print 'Sbajato',url
+		#print 'Scarico',url
+		if not os.path.isfile(tank+nome):
+			try:
+			urllib.urlretrieve(url,'/tmp/.temporaneo_score.pdf')
+			shutil.move('/tmp/.temporaneo_score.pdf',tank+nome)
+			print 'scaricato',nome
+			except:
+			print '                 Sbajato',url,nome
+			open('riscarica.txt','a').write(url+' '+nome+' \n')
+			time.sleep(30)
+		else:
+			print 'Salto',nome
